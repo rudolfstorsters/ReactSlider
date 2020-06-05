057845cfa7a7348de65d89878859c0b78f9a64a0
Rudolfs Torsters <torsters.rudolfs@gmail.com>
2020-06-05 22:46:19 +0300

    Go to slide functionlity

--- src/js/components/Slider/Slider.js
+++ src/js/components/Slider/Slider.js
@@ -1,42 +1,66 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 
 import ControlOverlay from "./ControlOverlay.js"
-import Slide from "./Slide.js" 
+import Slide from "./Slide.js"
 
 export default class Slider extends Component {
-    onLeftArrowClick = () =>{
-        console.log("left")
-    } 
-    onRightArrowClick = () =>{
-        console.log("right")
+    state = {
+        slideIndex: 0
+    }
+    goToSlide = (slideIndex) => {
+        if (this.scrollView) {
+            this.scrollView.scroll(window.innerWidth * slideIndex, 0)
+            this.setState({
+                slideIndex: slideIndex
+            })
+        }
+    }
+    onLeftArrowClick = () => {
+        this.goToSlide(this.state.slideIndex - 1)
+    }
+    onRightArrowClick = () => {
+        this.goToSlide(this.state.slideIndex + 1)
+
     }
     renderItem = (item, index) => {
-        return(
-         <Slide key={index}>
-            {item}
-         </Slide>
+        return (
+            <Slide key={index}>
+                {item}
+            </Slide>
         )
     }
-    render(){
+    render() {
         return (
-            <div style ={{
-                overflow: "hidden",
-                 position:"relative", 
-                 width: "100vw", 
-                 height: 500
-            }}>
+            <>
                 <div style={{
-                    display: "flex",
-                    flexDirection:"row",
-                    width: "fit-content" 
-                }}>
-                    {this.props.data.map(this.renderItem)} 
+                    position: "relative",
+                    width: "100vw",
+                }}
+                >
+                    <div style={{
+                        overflow: "scroll",
+                        scrollBehavior: "smooth",
+                        height: 500
+                    }}
+                        ref={(el) => { this.scrollView = el }}
+                    >
+                        <div style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            width: "fit-content",
+                        }}
+
+                        >
+                            {this.props.data.map(this.renderItem)}
+                        </div>
+                    </div>
+
                 </div>
-                <ControlOverlay 
-                    onLeftArrowClick={this.onLeftArrowClick} 
+                <ControlOverlay
+                    onLeftArrowClick={this.onLeftArrowClick}
                     onRightArrowClick={this.onRightArrowClick}
-                    />
-            </div>
+                />
+            </>
         )
     }
 }
